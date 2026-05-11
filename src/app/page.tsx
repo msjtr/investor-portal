b8c412c0dbd20b0299693a9b1d16cde9bc351622
msjtr <msjt301@gmail.com>
2026-05-11 03:11:13 +0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,1 +1,16 @@
-
+export default function HomePage() {
+  return (
+    <main
+      style={{
+        minHeight: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        fontSize: "40px",
+        fontWeight: "bold",
+      }}
+    >
+      Investor Portal 🚀
+    </main>
+  );
+}
