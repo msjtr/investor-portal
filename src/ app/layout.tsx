68034c95d8757528142199a4ffed4b3bc85f576c
msjtr <msjt301@gmail.com>
2026-05-11 03:48:14 +0300

    Add layout component for Investor Portal

--- src/ app/layout.tsx
+++ src/ app/layout.tsx
@@ -1,1 +1,18 @@
+import "./globals.css";
 
+export const metadata = {
+  title: "Investor Portal",
+  description: "Investor Dashboard",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
